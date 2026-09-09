7d29a79a082bcf87f6eb2cd921e4fba435a1ea46
David RT <Caldo_De_Pollos@hotmail.com>
2026-09-09 14:05:41 -0700

    Se agrego un docker para instalar todo lo necesario y un codigo que hace una consulta a la base de datos

--- conexion-mysql.py
+++ conexion-mysql.py
@@ -1,9 +1,10 @@
 import mysql.connector
 import time
+x = 3
 
 cnx = mysql.connector.connect(user ='paul' , password = 'Tec2062923', host = '172.17.0.1',database ='Prueba_BASE_DE_DATOSb') #original  host = '127.0.0.1'
 cursor = cnx.cursor()
-cursor.execute(f"SELECT * FROM sistemas_control WHERE id = 1;")
+cursor.execute(f"SELECT * FROM sistemas_control WHERE id = {x};")
 valor = cursor.fetchone()
 cnx.commit()
 cursor.close()
